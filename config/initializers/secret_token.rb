3e4b10a66b297ca01679cec1f4cf4b2121ea4eb3
justend <ipshow@gmail.com>
2013-03-27 19:44:40 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rmd::Application.config.secret_token = '0fab021609fb45fa79a4a9c47435569c7502ae66aea2252c4f39d3fa6f6d5bb8768f5e5bd342d3ab5f48b45117729824a164b12138faef3911864fef7b2aec90'
